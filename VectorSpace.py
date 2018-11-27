b426566b9dfaa50109a41259dee3ccf4da7f041d
JLGGitHub <leonardo.unillanos@gmail.com>
2018-11-26 23:47:43 -0500

    Refactory y clase VectorSpace

---
+++ VectorSpace.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 26 23:30:00 2018
+
+@author: Leonardo-PC
+"""
+import sys
+
+# VARIABLES 
+palabras = noticias = ""
+
+def main(args): 
+    
+
+# metodo main
+if __name__ == '__main__':
+    main(sys.argv)
+
